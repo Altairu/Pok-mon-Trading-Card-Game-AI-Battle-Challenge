979396c96b20f2a355391d91ec908d8fd322414c
-Altair- <106.nogu@gmail.com>
2026-06-19 11:03:00 +0900

    rlをメインエージェントとして設定

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def agent(obs_dict: dict) -> list[int]:
     if _agent_instance is None:
         # 遺伝的アルゴリズムを使用する進化計算エージェントをロードします。
         # 動作テストや元のベースラインに戻す場合は、引数を "random" に変更します。
-        _agent_instance = get_agent("evolutionary")
+        _agent_instance = get_agent("rl")
         #evolutionary" 遺伝的アルゴリズムを使用して進化させたルールベースエージェントです。
         #"pytorch_rl" 今回新しく実装した、PyTorchのニューラルネットワークモデルを使用してプレイする強化学習エージェントです。
         #"mcts" モンテカルロ木探索アルゴリズムを使用して数手先を予測しながらプレイする先読みエージェントです。
